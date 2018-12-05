6a87ee7d0b3643fff1d43cc2bf7430645ddf43b1
Nathan49 <nborneinca@gmail.com>
2018-12-04 18:40:56 -0800

    fuck yeah features done

--- assemblyassembler.py
+++ assemblyassembler.py
@@ -1,28 +1,30 @@
 import random
 file = open("assembly.txt", "w")
 file2 = open("hex.txt", "w")
-file3 = open("features.txt", "w")
+file3 = open("features.csv", "w")
 
-file3.write("add, sub, xor, mult, a = 0, b = 0, a = ffffffff, b = ffffffff, a < 0, b < 0, a > 0, b > 0, c = 0, c < 0, c > 0, unsigned overflow, signed overflow, negative sub result, xor zero, xor all ones, zero mult result, jump, data bypass\n")
+file3.write("add, sub, xor, mult, a = 0, b = 0, a = ffffffff, b = ffffffff, a < 0, b < 0, a > 0, b > 0, ass < 0, bs < 0, ass > 0, bs > 0, unsigned overflow, signed overflow, negative sub result, xor zero, xor all ones, zero mult result, jump, data bypass\n")
 
-feature_string = list('0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n')
+featureMap = {}
+
+featureList = ("add", "sub", "xor", "mult", "a = 0", "b = 0", "a = ffffffff", "b = ffffffff", "a < 0", "b < 0", "a > 0", "b > 0", "ass < 0", "bs < 0", "ass > 0", "bs > 0", "unsigned overflow", "signed overflow", "negative sub result", "xor zero", "xor all ones", "zero mult result", "jump", "data bypass")
 
 def I():
 	x = random.randint(0, 50)
 	if x < 15:
-		feature_string[0] = 1
+		featureMap["add"] = True
 		file2.write("005303b3\n")
 		return "add"
 	elif 15 <= x < 30:
-		feature_string[2] = 1
+		featureMap["sub"] = True
 		file2.write("405303b3\n")
 		return "sub"
 	elif 30 <= x < 40:
-		feature_string[4] = 1
+		featureMap["xor"] = True
 		file2.write("005343b3\n")
 		return "xor"
 	else:
-		feature_string[6] = 1
+		featureMap["mult"] = True
 		file2.write("025303b3\n")
 		return "mult"
 
@@ -30,51 +32,61 @@ def randomint(input):
 	x = random.randint(0, 100)
 	if x < 25:
 		if input == 'a':
-			feature_string[8] = 1
+			featureMap["a = 0"] = True
 		else:
-			feature_string[10] = 1
+			featureMap["b = 0"] = True
 		return "0"
 	elif 25 <= x < 50:
 		if input == 'a':
-			feature_string[12] = 1
-			feature_string[16] = 1
+			featureMap["a = ffffffff"] = True
+			featureMap["a > 0"] = True
+			featureMap["ass < 0"] = True
 		else:
-			feature_string[14] = 1
-			feature_string[18] = 1
+			featureMap["b = ffffffff"] = True
+			featureMap["b > 0"] = True
+			featureMap["bs < 0"] = True
 		return "ffffffff"
 	else:
-		s = str(hex(random.randint(0, 4294967295)))
-		if s[0] == "1":
+		s = random.randint(0, 0xffffffff)
+		featureMap["a > 0"] = True
+		featureMap["b > 0"] = True
+		if s & 0x80000000:
 			if input == 'a':
-				feature_string[16] = 1
+				featureMap["ass < 0"] = True
 			else:
-				feature_string[18] = 1
+				featureMap["bs < 0"] = True
 		else:
 			if input == 'a':
-				feature_string[20] = 1
+				featureMap["ass > 0"] = True 
 			else:
-				feature_string[22] = 1
+				featureMap["bs > 0"] = True
+		s = hex(s)
 		return s[2:]
 
-def feature_output(fstr, a, b):
+def feature_output(a, b):
 	a = int(a, 16)
 	b = int(b, 16)
-	if fstr[0] == 1: #add
-		fstr[30] = 0 #unsigned overflow
-		fstr[32] = 0 #signed overflow
-	elif fstr[2] == 1: #sub
-		if a - b < 0:
-			fstr[34] = 1 #negative sub result
-	elif fstr[4] == 1: #xor
+	ass = (a ^ 0xffffffff) + 1
+	bs = (b ^ 0xffffffff) + 1
+	if featureMap["add"] == True: #add
+		if a + b >= 2**32:
+			featureMap["unsigned overflow"] = True #unsigned overflow
+		if (ass > 0 and bs > 0 and ass + bs < 0) or (ass < 0 and bs < 0 and ass + bs > 0):
+			featureMap["signed overflow"] = True #signed overflow
+	elif featureMap["sub"] == True: #sub
+		if b > a:
+			featureMap["negative sub result"] = True #negative sub result
+	elif featureMap["xor"] == True: #xor
 		if a ^ b == 0:
-			fstr[36] = 1 #xor zero
+			featureMap["xor zero"] = True #xor zero
 		elif a ^ b == int("ffffffff", 16):
-			fstr[38] = 1 #xor all ones
+			featureMap["xor all ones"] = True #xor all ones
 	else: #mult
-		fstr[40] = int(fstr[8]) | int(fstr[10]) #zero mult result
+		featureMap["zero mult result"] = featureMap["a = 0"] | featureMap["b = 0"] #zero mult result
 
 for i in range(100):
-	feature_string = list('0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n')
+	for key in featureList:
+		featureMap[key] = False
 	file.write("lw $t0, $0, 12\n")
 	file2.write("00c02283\n")
 	file.write("lw $t1, $0, 16\n")
@@ -87,8 +99,8 @@ for i in range(100):
 	b = randomint('b')
 	file.write(b + "\n")
 	file2.write(b + "\n")
-	feature_output(feature_string, a, b)
-	file3.write(''.join(str(e) for e in feature_string))
+	feature_output(a, b)
+	file3.write(','.join([('1' if featureMap[feature] else '0') for feature in featureList]) + '\n')
 
 file.close()
 file2.close()
